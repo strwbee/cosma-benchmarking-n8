fe0f4db19354a7661d5b42798d00b6c9cb74b581
Abbey <abbey.m.noble@gmail.com>
2026-03-04 13:22:03 +0000

    Update A30 run paths to use /dine

--- BabelStream/run-scripts/run-cuda-a30.sh
+++ BabelStream/run-scripts/run-cuda-a30.sh
@@ -4,12 +4,10 @@
 #SBATCH --account=do015
 #SBATCH --nodes=1
 #SBATCH --time=00:05:00
-#SBATCH --output=gpu-results/babelstream-a30-%j.out
-#SBATCH --error=gpu-results/babelstream-a30-%j.err
-
-module load nvhpc/25.11
+#SBATCH --output=/dine/data/do009/dc-nobl3/babelstream/a30-%j.out
+#SBATCH --error=/dine/data/do009/dc-nobl3/babelstream/a30-%j.err
 
 echo "Running on: $(hostname)"
 nvidia-smi
 
-/cosma5/data/do009/dc-nobl3/babelstream-benchmark/BabelStream/build-cuda-sm80/cuda-stream --arraysize 134217728 --numtimes 100
+/dine/data/do009/dc-nobl3/babelstream/cuda-stream --arraysize 134217728 --numtimes 100 
